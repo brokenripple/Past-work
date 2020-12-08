17f015ec893227a49793e9dfa8c26608295bfa1d
Ripple Wu <70429096+brokenripple@users.noreply.github.com>
2020-12-09 00:31:11 +0800

    Update BERT_AutoTitling_CustomizedDataset.py

--- BERT_AutoTitling_CustomizedDataset.py
+++ BERT_AutoTitling_CustomizedDataset.py
@@ -40,11 +40,11 @@ from torch.utils.data import Dataset, DataLoader
 from bert_seq2seq.tokenizer import Tokenizer, load_chinese_base_vocab
 from bert_seq2seq.utils import load_bert, load_model_params, load_recent_model
 
-vocab_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/bert-base-chinese-pytorch_model.txt"  # roberta模型字典的位置
+vocab_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/bert-base-chinese-pytorch_model.txt"  
 word2idx, keep_tokens = load_chinese_base_vocab(vocab_path, simplfied=True)
-model_name = "bert"  # 选择模型名字
-model_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/bert-base-chinese-pytorch_model.bin"  # 模型位置
-recent_model_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/1111bert-base.bin"   # 用于把已经训练好的模型继续训练
+model_name = "bert"  
+model_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/bert-base-chinese-pytorch_model.bin"  
+recent_model_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/1111bert-base.bin"   
 model_save_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/1111bert-base.bin"
 batch_size = 16
 lr = 1e-5
@@ -65,24 +65,17 @@ lines = sents_tgt.split('\n')
 print (lines[99998])
 
 class BertDataset(Dataset):
-    """
-    针对特定数据集，定义一个相关的取数据的方式
-    """
     def __init__(self) :
-        ## 一般init函数是加载所有数据
         super(BertDataset, self).__init__()
         self.sents_src = read_file ("/content/drive/My Drive/ColabNotebooks/summary/extra_dict/train.src") 
         self.sents_tgt = read_file ("/content/drive/My Drive/ColabNotebooks/summary/extra_dict/train.tgt")
         self.sents_src = self.sents_src.split('\n')
         self.sents_tgt = self.sents_tgt.split('\n')
 
-        ## 拿到所有文件名字
         self.idx2word = {k: v for v, k in word2idx.items()}
         self.tokenizer = Tokenizer(word2idx)
 
     def __getitem__(self, i):
-        # 得到单个数据
-        # print(i)
         title = self.sents_tgt[i]
         content = self.sents_src[i]
         token_ids, token_type_ids = self.tokenizer.encode( content, title, max_length=maxlen)
@@ -97,14 +90,9 @@ class BertDataset(Dataset):
         return data_size
         
 def collate_fn(batch):
-    """
-    动态padding， batch为一部分sample
-    """
+
 
     def padding(indice, max_length, pad_idx=0):
-        """
-        pad 函数
-        """
         pad_indice = [item + [pad_idx] * max(0, max_length - len(item)) for item in indice]
         return torch.tensor(pad_indice)
 
@@ -121,51 +109,39 @@ def collate_fn(batch):
 class Trainer:
     def __init__(self):
 
-        # 判断是否有可用GPU
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print("device: " + str(self.device))
 
-        # 定义模型
         self.bert_model = load_bert(word2idx, model_name=model_name)
 
-        ## 加载预训练的模型参数
         #load_model_params(self.bert_model, model_path, keep_tokens=keep_tokens)
 
-        ## 加载已经训练好的模型，继续训练
+
         load_recent_model(self.bert_model, recent_model_path)
 
-        # 列印模型，将模型发送到计算设备(GPU或CPU)
         self.bert_model.to(self.device)
         print(self.bert_model) 
-        # 声明需要优化的参数
+
         self.optim_parameters = list(self.bert_model.parameters())
         self.optimizer = torch.optim.Adam(self.optim_parameters, lr=lr, weight_decay=1e-3)
-        #for name, param in self.bert_model.named_parameters():
-        #  if "decoder" not in name: # classifier layer
-        #    param.requires_grad = False
-        #  print (name, param.requires_grad)
-          
-        # 声明自定义的数据加载器
-  
+
         dataset = BertDataset()
         self.dataloader =  DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
     def train(self, epoch):
 
-        # 一个epoch的训练
+
         self.bert_model.train()
         self.iteration(epoch, dataloader=self.dataloader, train=True)
     
     def save(self, save_path):
-        """
-        保存模型
-        """
+
         torch.save(self.bert_model.state_dict(), save_path)
         print("{} saved!".format(save_path))
 
     def iteration(self, epoch, dataloader, train=True):
         total_loss = 0
-        start_time = time.time() ## 得到当前时间
+        start_time = time.time() 
         step = 0
         report_loss = 0
         for token_ids, token_type_ids, target_ids in tqdm(dataloader,position=0, leave=True):
@@ -191,34 +167,27 @@ class Trainer:
             token_ids = token_ids.to(self.device)
             token_type_ids = token_type_ids.to(self.device)
             target_ids = target_ids.to(self.device)
-            
-            # 因为传入了target标签，因此会计算loss并且返回
+         
             predictions, loss = self.bert_model(token_ids,
                                                 token_type_ids,
                                                 labels=target_ids,
                                                 device=self.device
                                                 )
             report_loss += loss.item()
-            # 反向传播
+
             if train:
-                # 清空之前的梯度
+
                 self.optimizer.zero_grad()
-                # 反向传播, 获取新的梯度
                 loss.backward()
-                # 用获取的梯度更新模型参数
                 self.optimizer.step()
 
-            # 为计算当前epoch的平均loss
             total_loss += loss.item()
 
         end_time = time.time()
         spend_time = end_time - start_time
-        # 打印训练信息
         print("epoch is " + str(epoch)+". loss is " + str(total_loss) + ". spend time is "+ str(spend_time))
-        # 保存模型
         self.save(model_save_path)
 
-# 测试一下自定义数据集
     
     #vocab_path = "/content/drive/My Drive/ColabNotebooks/summary/state_dict/vocab.txt" 
     #sents_src = read_file ("/content/drive/My Drive/ColabNotebooks/summary/extra_dict/train.src") 
